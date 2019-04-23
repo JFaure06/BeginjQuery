453dc8022c6ef57d8924cf4d6496107bd1a06cda
Julien FAURE <julien.faure26@gmail.com>
2019-04-23 14:38:55 +0200

    exercice 1 et 2

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,1 +1,9 @@
 console.log('exercice 1');
+
+/* Jqwery est une bibliotheque permettant de regrouper tous les navigateur internet et simplifier le code
+comparer à JS ou il faut le charger de condition pour etre compatible au differents navigatueurs
+ */
+
+$(document).ready(function(){
+    console.log('Je peux maintenant écrire du code jQuery');
+});
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,27 @@
 console.log('exercice 2');
 
+$(document).ready(() => {
+
+    // alert('test');
+
+    $('#title');//.css('background-color', 'yellow');
+
+    $('.sidenav');//.css('background-color', 'green');
+
+    $('footer');//.css('background-color', 'blue');
+
+    $('.glyphicon-search');//.css('background-color', 'red');
+
+    $('.glyphicon');//.css('background-color', 'grey');
+
+    $('.label-success');//.css('color', 'red');
+
+    $('p, .intro');//.css('background-color', 'grey');
+
+    $('img, .glyphicon-time');//.hide();
+
+    $('ul, li:first');//.css('color', 'yellow');
+
+    $('p:odd');//.css('background-color', '#bbbbff')
+
+});
