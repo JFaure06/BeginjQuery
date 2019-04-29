49ee450a0d7c2e670a21edeecff3c194c607a3a8
Julien FAURE <julien.faure26@gmail.com>
2019-04-29 16:15:45 +0200

    exercice 7 base fini

--- exo-7/script.js
+++ exo-7/script.js
@@ -1,5 +1,22 @@
 console.log("exercice 7");
 
-function getData() {
-  return data; // data is defined in DATA.js file
-}
+$(document).ready(() => {
+
+    function getData() {
+        return data; // data is defined in DATA.js file
+    };
+
+    function addLineToTable(line) {
+
+        $('#table tbody').append('<tr><td>' + line.email + '</td><td>' + line.name.last + '</td><td>' + line.name.first + '</td><td>' + line.phone + '</td></tr>');
+
+    }
+
+    var test = getData();
+
+    $.each(test, function () {
+
+        addLineToTable(this);
+    });
+
+});
